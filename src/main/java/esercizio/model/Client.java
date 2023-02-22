f2bd1ac0d1d585f6dd80f23ed50242b241fddcad
Fabio <depalma.fabio95@gmail.com>
2023-02-22 17:58:32 +0100

    Esercizio 4 da finire

--- src/main/java/esercizio/model/Client.java
+++ src/main/java/esercizio/model/Client.java
@@ -1,7 +1,37 @@
 package esercizio.model;
 
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+
 public class Client {
-    ObservableList<Email> list;
+    //lista di mail osservabile
+    ObservableList<Email> listMailObs;
+
+    //listMail con property cosi possiamo fare il binding
+    ListProperty<Email> emailList;
+    StringProperty utente;
+
+    public Client(String indirizzoMail){
+        this.listMailObs = FXCollections.observableList(new ArrayList<>());
+        this.emailList = new SimpleListProperty<>();
+        this.emailList.set(listMailObs);
+        this.utente = new SimpleStringProperty();
+    }
+
+    //Indirizzo dell'utente
+    public StringProperty utenteProperty(){
+        return utente;
+    }
+
+    //lista di mail
+    public ListProperty<Email> emailListProperty(){
+        return emailList;
+    }
+
 }
--- src/main/java/esercizio/model/Email.java
+++ src/main/java/esercizio/model/Email.java
@@ -5,18 +5,32 @@ import javafx.collections.ObservableList;
 import java.util.List;
 
 public class Email {
-   private String idUtente;
    private Person mittente;
    private List<Person> dest;
    private String titolo;
    private String testo;
 
 
-    public Email(String idUtente, Person mittente, List<Person> dest, String titolo, String testo) {
+    public Email(Person mittente, List<Person> dest, String titolo, String testo) {
         this.mittente = mittente;
         this.dest = dest;
         this.titolo = titolo;
         this.testo = testo;
     }
 
+    public Person getMittente() {
+        return mittente;
+    }
+
+    public List<Person> getDest() {
+        return dest;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
 }
